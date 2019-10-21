765c1c7b9a8b7db8fd804e0f4f84222fb4aaa316
Rida <ak6401108@gmail.com>
2019-10-21 23:24:59 +0500

    My First commit

---
+++ 6.py
@@ -0,0 +1,77 @@
+python
+>>>print(4)
+4
+
+>>>print type('Hello world')
+ <class 'str'>
+>>>type (17)
+<class 'int'>
+>>>type (17)
+<class 'str'>
+>>>type (3.2)
+class<'str'>
+
+>>>print(1,000,000)
+100
+
+print(1)
+x=2
+print(x)
+
+2*(3-1)
+2*2
+4
+
+(1+1)**(5-2)
+2**(5-2)
+2**3
+8
+
+5-3-1
+2-1
+1
+
+>>>quotient=7//3
+>>>print(quotient)
+2
+>>>remainder=7%3
+>>>print(remainder)
+1
+
+>>>first=10
+>>>second=15
+>>>print(first+second)
+25
+>>>first='100'
+>>>second='150'
+>>>print(first+second)
+100150
+
+>>>first='Test'
+>>>second=3
+>>>print(first*second)
+Test Test Test
+
+>>>inp=input()
+some silly stuff
+
+>>>name=input('what is your name?/n')
+what is your name?
+Rida
+>>>print(name)
+Rida
+
+hours=35.0
+rate=12.50
+pay=hours*rate
+print(pay)
+
+>>>name=input("what is your name?/n")
+what is your name?
+Rida
+>>>print("Hello",name,"!")
+
+>>>hours=int(input("Enterhours:\n"))
+>>>rate=float(input("Enter rate:\n"))
+>>>salary=hours*rate
+>>>print(salary)
